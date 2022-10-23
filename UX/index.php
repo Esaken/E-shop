3d1b70a5dd163b9e590e74f41c106900e3994a9f
kennedy esau <49650759+Esaken@users.noreply.github.com>
2022-10-22 23:18:56 -0700

    22/10/2022

--- UX/index.php
+++ UX/index.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../css/styles.css">
     <title>Home</title>
   </head>
   <body>
@@ -27,45 +27,113 @@
 <div class="row">
   <div class="col-md-10">
     <!-- Products section  -->
-    <div class="col-md-4">
+    <div class="col-md-4 mb-2">
     <div class="card">
   <img src="../images/shoe1.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
   </div>
 </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4 mb-2">
 
     <div class="card">
-  <img src="../images/shoe1.jpg" class="card-img-top" alt="...">
+  <img src="../images/shoe2.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
   </div>
 </div>
 
     </div>
+
+    <div class="col-md-4 mb-2">
+
+    <div class="card">
+  <img src="../images/shoe3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+
+    </div>
+
 
     <div class="col-md-4">
 
     <div class="card">
-  <img src="../images/shoe1.jpg" class="card-img-top" alt="...">
+  <img src="../images/shoe4.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
   </div>
 </div>
 
     </div>
+
   </div>
-  <div class="col-md-2">
+  <div class="col-md-2 bg-secondary p-0 mr-10px">
     <!-- Side nav -->
-    
+    <!-- brand to be displayed -->
+    <ul class="navbar-nav me-auto text-center">
+
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light"><h4>Important Brands</h4></a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand1</a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand2</a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand3</a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand4</a>
+      </li>
+
+    </ul>
+  
+  <!-- categories to be displayed -->
+  <ul class="navbar-nav me-auto text-center">
+
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Categories 1</a>
+        </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Categories 2</a>
+        </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Categories 3</a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Categories 4</a>
+      </li>
+
+    </ul>
+  
   </div>
 </div>
 
