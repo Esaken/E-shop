f7aeb8d5528f3dda7ac8d01e58c6534113b9e037
kennedy esau <49650759+Esaken@users.noreply.github.com>
2022-10-26 13:20:17 -0700

    connecting categories to gallery- there is a problem

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,7 +1,17 @@
 <?php
 include('../includes/connect.php');
-?>
+if(isset($_POST['insert_cat'])){
+    $category_title=$_POST['cat_title]'];
+    $insert_query="insert into 'categories' (category_title) values('$category_title')";
+    $result=mysqli_query($con, $insert_query);
+    if($result){
+        echo "<script>alert('Category has been inserted successfully')</script>";
+    }
 
+
+
+}
+?>
 
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
@@ -14,7 +24,7 @@ include('../includes/connect.php');
 
     <div class="input-group w-10 mb-2 m-auto">
        
-        <!-- <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert Categories"> -->
-        <button class="bg-info p-2 my-3 border-0">Insert Categories</button>
+         <input type="submit" class="bg-info border-0 my-3" name="insert_cat" value="Insert Categories"> 
+        
     </div>
 </form>
